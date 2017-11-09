f122b679dc8a18b0a884c5c8046ff898127fc422
cdzm5211314 <123309778@qq.com>
2017-11-09 10:40:59 +0800

    项目中使用JPA注解完成实体的映射

---
+++ src/main/java/com/chen/bos/domain/User.java
@@ -0,0 +1,96 @@
+package com.chen.bos.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="t_user")
+//使用lombok 注解@Data 生成setter和getter方法
+@Data
+public class User {
+	
+	@Id
+	// 使用hibernate 注解定义生成器
+	@GenericGenerator(name = "uuidGenerator", strategy = "uuid")
+	@GeneratedValue(generator = "uuidGenerator")
+	private String id;
+	@Column(name = "username", length = 20)
+	private String username;
+	private String password;
+	private Double salary;
+	// 针对日期
+	@Temporal(TemporalType.DATE)
+	private Date birthday;
+	private String gender;
+	private String station;
+	private String telephone;
+	private String remark;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getStation() {
+		return station;
+	}
+	public void setStation(String station) {
+		this.station = station;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+	
+}
